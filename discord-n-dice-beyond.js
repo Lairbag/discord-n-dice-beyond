386f313ea9a3544348197564989221a66748f5de
Gabriel <gabriel.desa@gmail.com>
2020-04-27 19:29:26 +0200

    Ajout des boutons de lancé de dès sur les compétences
    - création de addSkillDice
    - création de initDiscordNDiceBeyond qui attend le chargement des compétences avant d'ajouter les dès
    - maj des manifest pour avoir accès à plus d'icones

--- discord-n-dice-beyond.js
+++ discord-n-dice-beyond.js
@@ -1,17 +1,80 @@
-addButton();
+document.addEventListener('DOMContentLoaded', initDiscordNDiceBeyond());
 
-function addButton(){    
+function initDiscordNDiceBeyond(){
+    console.log("initDiscordNDiceBeyond");
+    
+    var millisecondsToWait = 3000;
+    setTimeout(function() {
+        var skillsLoaded = document.querySelectorAll(".ct-signed-number").length > 0;
+        if(skillsLoaded)
+        {
+            addDice();
+        }
+        else{
+            console.log("D&D is still loading, can't load dice.")
+            initDiscordNDiceBeyond();
+        }
+    }, millisecondsToWait);
+}
 
+function addDice(){
     var browserManager = new BrowserManagerFactory().manager;
     var dice = new Dice();
+
+    addMainDie(browserManager, dice);
+    addSkillDice(browserManager, dice);
+
+    window.onresize = function(){console.log("resize");;addSkillDice(browserManager, dice);}
+}
+
+function addSkillDice(browserManager, dice){
+    var diceIconUrl = browserManager.extensionGetUrl("./icons/d20-16.png");    
+    var i =0;
+    document.querySelectorAll(".ct-signed-number").forEach(element => {
+        var skillMod = "";
+        element.childNodes.forEach(child =>{
+            skillMod += child.innerText;
+        });
+
+        var id = "diceButton_"+(i++);   
+        if(document.querySelector("#"+id) === null){
+            var htmlButton = '<div id="'+id+'" style="cursor: pointer; z-index 60002"><img src="'+diceIconUrl+'" alt="Roll the dice"></img></div>';
+
+            element.insertAdjacentHTML("beforeend", htmlButton); 
+    
+            document.querySelector("#"+id).onclick = function(e){
+                //hideSideBar();
+                
+                dice.roll(browserManager, skillMod);            
+                //e.preventDefault();
+            };
+        }        
+    });
+}
+
+// function hideSidebar(){
+//     var sideBar = document.querySelector('[data-original-title="Hide Sidebar"]');
+//     if(sideBar)
+//     {
+//         sideBar.click();
+//     }
+//     else{
+//         var millisecondsToWait = 1000;
+//         setTimeout(function() {
+//             hideSidebar();
+//         }, millisecondsToWait);
+//     }
+// }
+
+function addMainDie(browserManager, dice){    
 
     var diceIconUrl = browserManager.extensionGetUrl("./icons/d20-48.png");
     var htmlButton = '<div id="diceButton" style="cursor: pointer; position: fixed; bottom: 5px; left: 5px; z-index 60002"><img src="'+diceIconUrl+'" alt="Roll the dice"></img></div>';
     document.querySelector("#site-main").insertAdjacentHTML("beforeend", htmlButton);
 
     document.querySelector("#diceButton").onclick = function(e){
-        dice.roll(browserManager);
-    }    
+        dice.roll(browserManager, null);
+    }
 }
 
 
