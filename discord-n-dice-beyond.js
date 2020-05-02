c9bd3a3af1d1745208b4c835a46019fabe29ab93
Gabriel <gabriel.desa@gmail.com>
2020-05-02 23:35:11 +0200

    Gestion des dés de dommage
    - refacto de dice.js pour injecter le nombre et le type de dés
    - ajout de addDamageDice

--- discord-n-dice-beyond.js
+++ discord-n-dice-beyond.js
@@ -22,8 +22,12 @@ function addDice(){
 
     addMainDie(browserManager, dice);
     addSkillDice(browserManager, dice);
+    addDamageDice(browserManager, dice);
 
-    window.onresize = function(){addSkillDice(browserManager, dice);}
+    window.onresize = function(){
+        addSkillDice(browserManager, dice);
+        addDamageDice(browserManager, dice);
+    }
 }
 
 function addSkillDice(browserManager, dice){
@@ -50,19 +54,76 @@ function addSkillDice(browserManager, dice){
             skillMod += child.innerText;
         });
 
-        var id = "diceButton_"+(i++);   
+        var id = "diceButton_skill_"+(i++);   
         if(document.querySelector("#"+id) === null){
-            var htmlButton = '<div id="'+id+'" style="cursor: pointer; z-index 60002"><img src="'+diceIconUrl+'" alt="Roll the dice"></img></div>';
+            var htmlButton = '<div id="'+id+'" style="cursor: pointer; z-index 60002"><img src="'+diceIconUrl+'" alt="Roll the skill die"></img></div>';
 
             element.insertAdjacentHTML("beforeend", htmlButton); 
     
             document.querySelector("#"+id).onclick = function(e){
                 self.hideSidebar();
                 
-                dice.roll(browserManager, skillMod);            
-                //e.preventDefault();
+                dice.roll(browserManager, 1, 20, skillMod);
             };
         }        
+    });
+}
+
+function addDamageDice(browserManager, dice){
+    var self = this;
+    self.hideSidebar = function(){
+        var sideBar = document.querySelector('[data-original-title="Hide Sidebar"]');
+        if(sideBar)
+        {
+            sideBar.click();
+        }
+        else{
+            var millisecondsToWait = 1000;
+            setTimeout(function() {
+                hideSidebar();
+            }, millisecondsToWait);
+        }
+    }
+
+    var diceIconUrl = browserManager.extensionGetUrl("./icons/d20-16.png");    
+    var i =0;
+    document.querySelectorAll(".ct-damage__value").forEach(element => {
+        var patt = new RegExp("^[0-9]*d[0-9]*[+-][0-9]*");
+        if(patt.test(element.innerHTML))
+        {         
+            try{
+                var numberOfDice = 1;
+                patt = new RegExp("^[0-9]*");
+                if(patt.test(element.innerHTML)){
+                    numberOfDice = patt.exec(element.innerHTML)[0];
+                }
+    
+                patt = new RegExp("d[0-9]*");
+                var diceKind = parseInt(patt.exec(element.innerHTML)[0].substr(1));
+    
+                var mod = "";
+                patt = new RegExp("[+-][0-9]*");
+                if(patt.test(element.innerHTML)){
+                    mod = patt.exec(element.innerHTML);
+                }
+                
+                var id = "diceButton_damage_"+(i++);   
+                if(document.querySelector("#"+id) === null){
+                    var htmlButton = '<div id="'+id+'" style="cursor: pointer; display: inline-block"><img src="'+diceIconUrl+'" alt="Roll the damage die"></img></div>';
+    
+                    element.insertAdjacentHTML("beforeend", htmlButton); 
+            
+                    document.querySelector("#"+id).onclick = function(e){
+                        self.hideSidebar();
+                        dice.roll(browserManager, numberOfDice, diceKind, mod);
+                    };
+                }
+            }   
+            catch(e)
+            {
+                console.error(e);
+            }            
+        }          
     });
 }
 
@@ -73,6 +134,6 @@ function addMainDie(browserManager, dice){
     document.querySelector("#site-main").insertAdjacentHTML("beforeend", htmlButton);
 
     document.querySelector("#diceButton").onclick = function(e){
-        dice.roll(browserManager, null);
+        dice.roll(browserManager, 4, 20, null);
     }
 }
